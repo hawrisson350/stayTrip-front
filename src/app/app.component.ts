84a90e51b493f6c106f8c2d4924961562ec46cba
Hawrisson Avendaño <hawrisson.avendano@zabud.com.co>
2024-02-03 12:54:15 -0500

    Setup inicial repo with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './views/partials/footer/footer.component';
+import { TopBarComponent } from './views/partials/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, TopBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { FormFieldModule } from './form-field/form-field.module';
+// import { ButtonsModule } from './buttons/buttons.module';
+// import { AlertModule } from './alert/alert.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ],
+  exports:[
+    // FormFieldModule,
+    // ButtonsModule,
+    // AlertModule,
+  ]
+})
+export class SharedModule { }
